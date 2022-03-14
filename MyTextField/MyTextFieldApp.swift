03233da00670f680a0f23993e38d38b341f4ce4f
박힘찬 <himchan05@naver.com>
2022-03-14 21:20:53 +0900

    Initial Commit

---
+++ MyTextField/MyTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTextFieldApp.swift
+//  MyTextField
+//
+//  Created by 박힘찬 on 2022/03/14.
+//
+
+import SwiftUI
+
+@main
+struct MyTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
