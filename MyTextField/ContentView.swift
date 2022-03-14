1bfd5e8414609f85e71fd2c84846405c0028117e
박힘찬 <himchan05@naver.com>
2022-03-14 21:39:18 +0900

    My TextField

--- MyTextField/ContentView.swift
+++ MyTextField/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing : 10) {
+            HStack {
+                TextField("아이디를 입력하세요", text: $username)
+                    .textFieldStyle(.roundedBorder)
+                    .autocapitalization(.none) // first char lower
+                    .disableAutocorrection(true) // disable autocorrect
+                Button(action: {
+                    self.username = ""
+                }) {
+                    if self.username.count > 0 {
+                        Image(systemName: "x.circle")
+                            .foregroundColor(Color.red)
+                    }
+                }
+            }
+            HStack {
+                SecureField("비밀번호를 입력하세요", text: $password)
+                    .textFieldStyle(.roundedBorder)
+                    .autocapitalization(.none) // first char lower
+                    .disableAutocorrection(true) // disable autocorrect
+                Button(action: {
+                    self.password = ""
+                }) {
+                    if self.password.count > 0 {
+                        Image(systemName: "x.circle")
+                            .foregroundColor(Color.red)
+                    }
+                }
+            }
+            Text("password: \(self.password)")
+        }
+        .padding()
+        .background(Color.green)
+        .cornerRadius(20)
     }
 }
 
